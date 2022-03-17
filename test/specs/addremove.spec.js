28dc582bd2885b235cc3e7b0c0e808a144c4856b
Mac Mini 16 <macmini16@Macs-Mac-mini-16.local>
2022-03-17 11:59:08 -0700

    first commit

---
+++ test/specs/addremove.spec.js
@@ -0,0 +1,17 @@
+const AddremovePage = require('../pageobjects/addremove.page.js');
+
+
+describe('My Addremove application', () => {
+    it(''), async () => {
+        await AddremovePage.open();
+
+        await browser.pause(2000);
+
+        await (AddremoveElement).click()
+
+    
+        
+        
+            
+
+
